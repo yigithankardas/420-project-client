cf2b181f9f1ddbb80066f0ee11450d0d3ded7043
yigithankardas <y.kardas@etu.edu.tr>
2023-12-02 18:10:16 +0300

    (#2) CHG: Module quits immediately if it's been called from other modules. (#2) REM: finally statement has been removed.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+if __name__ != "__main__":
+    exit(1)
+
 import socket
 import time
 import signal
@@ -9,28 +12,23 @@ PORT = 5555
 
 
 def connectServer():
+    print(f'My ID: {uniqueId}')
     while True:
-        print(f'doing stuff with {uniqueId}')
-        time.sleep(1)
+        requestedId = input('Enter ID: ')
 
+def signalHandler(signal, frame):
+    clientSocket.close()
+    sys.exit(1)
 
-if __name__ == "__main__":
-    def signalHandler(signal, frame):
-        clientSocket.close()
-        sys.exit(1)
+signal.signal(signal.SIGINT, signalHandler)
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((HOST, PORT))
+clientSocket.settimeout(5)
 
-    signal.signal(signal.SIGINT, signalHandler)
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((HOST, PORT))
-
-    clientSocket.settimeout(5)
+while True:
     try:
         uniqueId = int(clientSocket.recv(1024).decode())
-        print(f"Connected to the server. Assigned ID: {uniqueId}")
+        print(f"Connected to the server. Assigned ID:{uniqueId}")
         connectServer()
-
     except socket.timeout:
-        print("Timeout occurred. Connection to the server timed out.")
-
-    finally:
-        clientSocket.close()
+            print("Timeout occurred. Connection to the server timed out.")
